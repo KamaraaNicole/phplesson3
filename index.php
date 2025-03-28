6e7e26c68979bae8eadefe8e2b184fab5a00201b
KamaraaNicole <kamara.mitchell@students.jmcss.org>
2025-03-28 09:50:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
 
-  <body>
+  <body> 
+    <?php
 
-  <form method="GET"> 
-     <input type="text" name="person"> 
-     <button>SUBMIT</button>
-</form>
+     //string 
+     $name = 'coding is fun';
 
+     //Integer
+     $name = 20;
 
-<?php
-//in the datbase  its lookong for a name to put in front of "is a pretty girl."
-   $name = $_GET['person'];
-   //
-   echo $name. " is a pretty girl";
-?>
+     //Float
+     $name = 20.5784;
+
+     //BooLean
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+     //Array
+     $names = array("Kamara","Nicole","Mitchell");
+     echo $names['1']
+ ?>
+
+
+    
   </body>
 </html>
